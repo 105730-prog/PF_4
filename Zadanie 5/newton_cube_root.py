0a577359e77734a0d2c2a80b66a715b95e4fcb33
Arek Jaw <73388566+105730-prog@users.noreply.github.com>
2023-06-16 18:41:44 +0200

    Delete Zadanie 5 directory

--- Zadanie 5/newton_cube_root.py
+++
@@ -1,7 +0,0 @@
-def newton_cube_root(k, epsilon):
-  guess = k
-  while(((1/3)*(2*guess + k/guess**2))**3 - k >= epsilon):
-    guess = (1/3)*(2*guess + k/guess**2)
-    print(f"Cube root of {k} is about {guess}")
-
-newton_cube_root(216, .001)
